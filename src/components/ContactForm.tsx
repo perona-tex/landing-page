cc24bd64a3ce4dc9c5449b904b6f08a79488bfce
Marcho Handoko <106444254+Marchosh@users.noreply.github.com>
2024-04-25 15:21:32 -0700

    update

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -121,7 +121,6 @@ const ContactForm = () => {
           <textarea
             name="message"
             id="message"
-            rows="4"
             required
             className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
           ></textarea>
